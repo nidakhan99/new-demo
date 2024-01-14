7e0c92a5f11d4fd865c8bcfdb2cb4efcd3bc965a
nidakhan99 <153353850+nidakhan99@users.noreply.github.com>
2024-01-14 14:59:54 +0530

    Update mavenHelper.groovy

--- vars/mavenHelper.groovy
+++ vars/mavenHelper.groovy
@@ -4,3 +4,7 @@ def runMavenTests(String options = '') {
     // Execute Maven test command
     sh "mvn test ${options}"
 }
+def runDependencyCheck() {
+    // Run OWASP Dependency-Check
+    sh 'mvn org.owasp:dependency-check-maven:check'
+}
